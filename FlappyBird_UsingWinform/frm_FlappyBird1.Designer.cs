c2844d28ea1c50ef774f5ac9b7a4020dcbb9f6c1
StringShiro <ph.tandat10202@gmail.com>
2022-08-06 11:43:48 +0700

    Very first step of Flappy Bird

---
+++ FlappyBird_UsingWinform/frm_FlappyBird1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace FlappyBird_UsingWinform
+{
+    partial class frm_FlappyBird
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_FlappyBird));
+            this.Picbox_ground = new System.Windows.Forms.PictureBox();
+            this.Picbox_bird = new System.Windows.Forms.PictureBox();
+            this.Picbox_pipedown = new System.Windows.Forms.PictureBox();
+            this.Picbox_pipeup = new System.Windows.Forms.PictureBox();
+            this.lbl_Score = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.Picbox_pipedown1 = new System.Windows.Forms.PictureBox();
+            this.Picbox_pipeup1 = new System.Windows.Forms.PictureBox();
+            this.Picbox_point = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_pipedown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_pipeup)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_pipedown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_pipeup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_point)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Picbox_ground
+            // 
+            this.Picbox_ground.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Picbox_ground.Image = ((System.Drawing.Image)(resources.GetObject("Picbox_ground.Image")));
+            this.Picbox_ground.Location = new System.Drawing.Point(0, 401);
+            this.Picbox_ground.Name = "Picbox_ground";
+            this.Picbox_ground.Size = new System.Drawing.Size(584, 60);
+            this.Picbox_ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Picbox_ground.TabIndex = 0;
+            this.Picbox_ground.TabStop = false;
+            // 
+            // Picbox_bird
+            // 
+            this.Picbox_bird.Image = ((System.Drawing.Image)(resources.GetObject("Picbox_bird.Image")));
+            this.Picbox_bird.Location = new System.Drawing.Point(69, 199);
+            this.Picbox_bird.Name = "Picbox_bird";
+            this.Picbox_bird.Size = new System.Drawing.Size(42, 30);
+            this.Picbox_bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Picbox_bird.TabIndex = 0;
+            this.Picbox_bird.TabStop = false;
+            // 
+            // Picbox_pipedown
+            // 
+            this.Picbox_pipedown.Image = ((System.Drawing.Image)(resources.GetObject("Picbox_pipedown.Image")));
+            this.Picbox_pipedown.Location = new System.Drawing.Point(274, -24);
+            this.Picbox_pipedown.Name = "Picbox_pipedown";
+            this.Picbox_pipedown.Size = new System.Drawing.Size(70, 180);
+            this.Picbox_pipedown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Picbox_pipedown.TabIndex = 0;
+            this.Picbox_pipedown.TabStop = false;
+            // 
+            // Picbox_pipeup
+            // 
+            this.Picbox_pipeup.Image = ((System.Drawing.Image)(resources.GetObject("Picbox_pipeup.Image")));
+            this.Picbox_pipeup.Location = new System.Drawing.Point(274, 276);
+            this.Picbox_pipeup.Name = "Picbox_pipeup";
+            this.Picbox_pipeup.Size = new System.Drawing.Size(70, 189);
+            this.Picbox_pipeup.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Picbox_pipeup.TabIndex = 0;
+            this.Picbox_pipeup.TabStop = false;
+            // 
+            // lbl_Score
+            // 
+            this.lbl_Score.AutoSize = true;
+            this.lbl_Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lbl_Score.Location = new System.Drawing.Point(13, 13);
+            this.lbl_Score.Name = "lbl_Score";
+            this.lbl_Score.Size = new System.Drawing.Size(29, 31);
+            this.lbl_Score.TabIndex = 1;
+            this.lbl_Score.Text = "0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // Picbox_pipedown1
+            // 
+            this.Picbox_pipedown1.Image = ((System.Drawing.Image)(resources.GetObject("Picbox_pipedown1.Image")));
+            this.Picbox_pipedown1.Location = new System.Drawing.Point(486, -11);
+            this.Picbox_pipedown1.Name = "Picbox_pipedown1";
+            this.Picbox_pipedown1.Size = new System.Drawing.Size(70, 207);
+            this.Picbox_pipedown1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Picbox_pipedown1.TabIndex = 0;
+            this.Picbox_pipedown1.TabStop = false;
+            // 
+            // Picbox_pipeup1
+            // 
+            this.Picbox_pipeup1.Image = ((System.Drawing.Image)(resources.GetObject("Picbox_pipeup1.Image")));
+            this.Picbox_pipeup1.Location = new System.Drawing.Point(486, 310);
+            this.Picbox_pipeup1.Name = "Picbox_pipeup1";
+            this.Picbox_pipeup1.Size = new System.Drawing.Size(70, 163);
+            this.Picbox_pipeup1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Picbox_pipeup1.TabIndex = 0;
+            this.Picbox_pipeup1.TabStop = false;
+            // 
+            // Picbox_point
+            // 
+            this.Picbox_point.Image = ((System.Drawing.Image)(resources.GetObject("Picbox_point.Image")));
+            this.Picbox_point.Location = new System.Drawing.Point(298, 211);
+            this.Picbox_point.Name = "Picbox_point";
+            this.Picbox_point.Size = new System.Drawing.Size(24, 18);
+            this.Picbox_point.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Picbox_point.TabIndex = 2;
+            this.Picbox_point.TabStop = false;
+            // 
+            // frm_FlappyBird
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.Picbox_point);
+            this.Controls.Add(this.lbl_Score);
+            this.Controls.Add(this.Picbox_ground);
+            this.Controls.Add(this.Picbox_pipeup1);
+            this.Controls.Add(this.Picbox_pipedown1);
+            this.Controls.Add(this.Picbox_pipeup);
+            this.Controls.Add(this.Picbox_pipedown);
+            this.Controls.Add(this.Picbox_bird);
+            this.DoubleBuffered = true;
+            this.Name = "frm_FlappyBird";
+            this.Text = "Flappy Bird";
+            this.Load += new System.EventHandler(this.frm_FlappyBird_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_pipedown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_pipeup)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_pipedown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_pipeup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Picbox_point)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Picbox_ground;
+        private System.Windows.Forms.PictureBox Picbox_bird;
+        private System.Windows.Forms.PictureBox Picbox_pipedown;
+        private System.Windows.Forms.PictureBox Picbox_pipeup;
+        private System.Windows.Forms.Label lbl_Score;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox Picbox_pipedown1;
+        private System.Windows.Forms.PictureBox Picbox_pipeup1;
+        private System.Windows.Forms.PictureBox Picbox_point;
+    }
+}
+
---
+++ FlappyBird_UsingWinform/frm_FlappyBird1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace FlappyBird_UsingWinform
+{
+    public partial class frm_FlappyBird : Form
+    {
+        int gravity = 5;
+        int pipeSpeed = 4;
+        int gameScore = 0;
+        bool gameOver = true;
+
+        public frm_FlappyBird()
+        {
+            InitializeComponent();
+            Picbox_bird.BackColor = Color.Transparent;
+            Picbox_pipedown.BackColor = Color.Transparent;
+            Picbox_pipeup.BackColor = Color.Transparent;
+            Picbox_pipedown1.BackColor = Color.Transparent;
+            Picbox_pipeup1.BackColor = Color.Transparent;
+            Picbox_point.BackColor = Color.Transparent;
+        }
+
+        private void frm_FlappyBird_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Random rnd = new Random();
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            Picbox_bird.Top += gravity;
+            //pipe
+            Picbox_point.Left -= pipeSpeed;
+            Picbox_pipedown.Left -= pipeSpeed;
+            Picbox_pipeup.Left -= pipeSpeed;
+            Picbox_pipedown1.Left -= pipeSpeed;
+            Picbox_pipeup1.Left -= pipeSpeed;
+            //score
+            lbl_Score.Text = "Score:" + gameScore;
+
+            //tạo cột ngẫu nhiên && point
+            if(Picbox_point.Left < 80)
+            {
+                Picbox_point.Left = rnd.Next(400, 800);
+                gameScore++;
+                if (Picbox_pipedown.Left < -100 && Picbox_pipeup.Left < -100)
+                {
+                    Picbox_pipedown.Left = rnd.Next(400, 800);
+                    Picbox_pipeup.Left = rnd.Next(300, 800);
+                }
+                if (Picbox_pipedown1.Left < -250 && Picbox_pipeup1.Left < -250)
+                {
+                    Picbox_pipedown1.Left = rnd.Next(400, 700);
+                    Picbox_pipeup1.Left = rnd.Next(400, 700);
+                }
+            }
+           
+            //tạo event va chạm chủ thể là Picbox_Bird
+           if (Picbox_bird.Bounds.IntersectsWith(Picbox_ground.Bounds)
+               || Picbox_bird.Bounds.IntersectsWith(Picbox_pipeup.Bounds)
+               || Picbox_bird.Bounds.IntersectsWith(Picbox_pipedown.Bounds)
+               || Picbox_bird.Bounds.IntersectsWith(Picbox_pipeup1.Bounds)
+               || Picbox_bird.Bounds.IntersectsWith(Picbox_pipedown1.Bounds)
+               )
+            {
+                gameTimer.Stop();
+                lbl_Score.Text += " Game Over !!!";
+            }
+
+            //tăng độ khó game bằng cách tăng tốc độ xuất hiện các cột
+            Increase_Hard();
+        }
+
+        private void Increase_Hard()
+        {
+            if (gameScore > 10)
+            {
+                pipeSpeed = 8;
+                    gravity = 7;
+            }
+            if (gameScore > 20)
+            {
+                pipeSpeed = 10;
+                gravity = 8;
+            }
+            if (gameScore > 30)
+            {
+                pipeSpeed = 12;
+                gravity = 9;
+            }
+            if (gameScore > 40)
+            {
+                pipeSpeed = 14;
+                gravity = 10;
+            }
+        }
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space) {
+                gravity = -6;
+            }
+            else if (e.KeyCode == Keys.Enter) {
+                gameTimer.Start();
+                gameTimer.Enabled = true;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 5;
+            }
+        }
+    }
+}
